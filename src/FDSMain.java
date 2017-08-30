a07430eabaa8b548d6a57e38df17d468bcecc80a
RobsonAndrei <robsonsantos@10.132.249.47>
2017-08-29 22:13:29 -0300

    #RobsonAndrei

---
+++ src/FDSMain.java
@@ -0,0 +1,19 @@
+
+public class FDSMain {
+
+	public static void main(String[] args) {
+		// *******************
+		// Insira, no final deste m�todo, um coment�rio com
+		// o seu nome, e um println com o Hello, World, conform
+		// o exemplo abaixo.
+		System.out.println("Hello, World! Comunitario!\n");
+
+		// Michael da Costa M�ra
+		System.out.println("Hello, World! Comunitario! Michael\n");
+
+		// Robson Andrei
+		System.out.println("Hello, World! Comunitario! Robson Andrei  \n");
+
+	}
+
+}
